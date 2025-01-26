ef9b725e60ee3781c9a62a2fc38c4646fc185564
Melenkaa <aa.melenka@gmail.com>
2025-01-27 02:16:54 +0300

    Upload lab3

---
+++ src/main/java/ru/spbstu/telematics/java/RollerCoaster.java
@@ -0,0 +1,75 @@
+import java.util.concurrent.Semaphore;
+
+public class RollerCoaster {
+    private static final int M = 5;
+
+    private static Semaphore turnstile = new Semaphore(1);
+    private static Semaphore controller = new Semaphore(0);
+    private static Semaphore cart = new Semaphore(0);
+
+    private static int passengersOnPlatform = 0;
+
+    public static void main(String[] args) {
+        new Thread(new Turnstile()).start();
+        new Thread(new Controller()).start();
+        new Thread(new Cart()).start();
+    }
+
+    static class Turnstile implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    turnstile.acquire();
+                    synchronized (this) {
+                        passengersOnPlatform++;
+                        System.out.println("The passenger arrived on the platform. Passengers: " + passengersOnPlatform);
+                        if (passengersOnPlatform == M) {
+                            controller.release();
+                        } else {
+                            turnstile.release();
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    static class Controller implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    controller.acquire();
+                    System.out.println("The controller signals the departure of the cart.");
+                    cart.release();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    static class Cart implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    cart.acquire();
+                    System.out.println("The cart is leaving.");
+                    Thread.sleep(2000);
+
+                    synchronized (this) {
+                        passengersOnPlatform = 0;
+                        System.out.println("The cart returned. The passengers left the platform.");
+                        turnstile.release();
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
